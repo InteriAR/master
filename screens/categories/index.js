96c29710908dfd23944b00c24cfdd51fb8916189
magilo <53962625+magilo@users.noreply.github.com>
2019-11-26 12:55:22 -0500

    removed unused code

--- screens/categories/index.js
+++ screens/categories/index.js
@@ -1,22 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View, FlatList, ScrollView, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import axios from 'axios'
 import { wayfairAuth } from './../../secrets'
 
-const DATA = [
-    {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-    },
-    {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-    },
-    {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-    }
-]
 
 function Item({ category }) {
     return (
