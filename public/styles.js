a05d061f4a845ce04a8fdffa2c83398091db7e8a
Stefanie Reyes <sreyes8@binghamton.edu>
2019-12-03 12:56:52 -0500

    fix: design spacing

--- public/styles.js
+++ public/styles.js
@@ -50,7 +50,7 @@ const styles = {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 30
+    marginTop: 10
   },
   profileButtonText: {
     color: "#54320d"
